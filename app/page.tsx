e021c4c919beb8bb4b73f91452de53f04b79a602
Azizbek <Azizbek_emailingiz@example.com>
2026-04-27 18:24:47 +0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-import NotFoundPage from './notFound/page' 
-import PrivacyPolicy from './PrivacyPolicy/page'
 
 function Page() {
   return (
     <div>
-      <NotFoundPage/>
-      <PrivacyPolicy/>
+    
     </div>
   )
 }
